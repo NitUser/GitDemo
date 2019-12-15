59ceaacf948bb0a0c88becc2e54762f134d49561
NitUser <gond.angad@gmail.com>
2019-12-15 13:17:59 +0530

    add some thing

--- src/com/app/test/MainTest.java
+++ src/com/app/test/MainTest.java
@@ -2,13 +2,15 @@ package com.app.test;
 
 public class MainTest {
 
-	public void addData(String data) {
+	public  static void addData(String data) {
 		if(data==null) {
 			System.out.println("No any Work");
 		}
 		System.out.println("Some work is here");
+		
 	}
 	public static void main(String[] args) {
+		addData("Data");
      System.out.println("Mian Method");
 	}
 }
