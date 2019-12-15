0669ada0a50fce63fe30756363cf779b36b23010
NitUser <gond.angad@gmail.com>
2019-12-15 13:13:50 +0530

    Added one method

---
+++ src/com/app/test/MainTest.java
@@ -0,0 +1,14 @@
+package com.app.test;
+
+public class MainTest {
+
+	public void addData(String data) {
+		if(data==null) {
+			System.out.println("No any Work");
+		}
+		System.out.println("Some work is here");
+	}
+	public static void main(String[] args) {
+     System.out.println("Mian Method");
+	}
+}
